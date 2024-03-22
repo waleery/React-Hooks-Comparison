24fed4594bdc64d114ea4520e8fdacdfbefb1056
waleery <walenda1997@gmail.com>
2024-03-22 22:50:20 +0100

    feat(App): add function to display selected hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
+import { useState } from "react";
 import "./App.css";
 import Navbar from "./Components/Navbar/navbar";
 import UseState from "./Components/useState/useState";
 
 function App() {
+    const [selectedHook, setSelectedHook] = useState("useState");
+
+    const displaySelectedHook = () => {
+        switch (selectedHook) {
+            case "useState":
+                return <UseState />;
+            default:
+                return <UseState />;
+        }
+    };
     return (
         <>
             <Navbar />
-            <main className="main-cointainer">
-                <UseState/>
-            </main>
+            <main className="main-cointainer">{displaySelectedHook()}</main>
         </>
     );
 }
