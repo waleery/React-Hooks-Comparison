3d9f4bb9fc283e73dd53f6a6d42c40b67b1d1536
waleery <walenda1997@gmail.com>
2024-03-22 14:52:17 +0100

    feat(App): add main container

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,16 @@ import "./App.css";
 import Navbar from "./Components/Navbar/navbar";
 
 function App() {
-    return <Navbar />;
+    return (
+        <>
+            <Navbar />
+            <main className="main-cointainer">
+                <div className="element">
+                    Element
+                </div>
+            </main>
+        </>
+    );
 }
 
 export default App;
