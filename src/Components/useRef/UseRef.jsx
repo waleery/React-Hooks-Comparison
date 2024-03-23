11b737e5fa38a78e48cdf2a2b158833d25367a67
waleery <walenda1997@gmail.com>
2024-03-23 22:30:11 +0100

    feat(Hooks): add useRef

---
+++ src/Components/useRef/UseRef.jsx
@@ -0,0 +1,57 @@
+import { useEffect, useRef, useState } from "react";
+import { render } from "react-dom";
+
+const UseRef = () => {
+    const [name, setName] = useState("");
+
+    const rednerCount = useRef(1);
+    const inputRef = useRef();
+    const prevName = useRef("");
+
+    useEffect(() => {
+        rednerCount.current = rednerCount.current + 1;
+    });
+
+    useEffect(() => {
+        prevName.current = name;
+    }, [name]);
+
+    const focus = () => {
+        inputRef.current.focus();
+    };
+    return (
+        <div className="hookContainer">
+            <div className="description" style={{ maxWidth: "40%" }}>
+                <span className="title">useRef</span>
+                <p className="info">
+                    With useRef in React, we can reference elements in the DOM
+                    tree.
+                </p>
+                <p className="info">Update ref value won't triger rerender.</p>
+                <p>
+                    In refs, we can store the previous value. After updating the
+                    state, we update the ref with this value. Updating the ref
+                    won't trigger a rerender of the component, so the new value
+                    of the ref will not be displayed. The new value of the ref
+                    will be displayed after the next rerender of the component.
+                </p>
+                <p></p>
+            </div>
+            <input
+                ref={inputRef}
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+            />
+            <span>
+                My name is: {name}, and it used to be: {prevName.current}{" "}
+            </span>
+
+            <span>Component rendered: {rednerCount.current} times </span>
+
+            <div className="buttons">
+                <button onClick={focus}>Focus on input</button>
+            </div>
+        </div>
+    );
+};
+export default UseRef;
