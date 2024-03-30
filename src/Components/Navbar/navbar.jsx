3ae6f77ffea09259ea12afe4b3f6048e0253570a
waleery <walenda1997@gmail.com>
2024-03-31 00:13:06 +0100

    feat(Navbar): add logic to dynamicly show/hide hooks which do not fit

--- src/Components/Navbar/navbar.jsx
+++ src/Components/Navbar/navbar.jsx
@@ -1,41 +1,24 @@
-import { useLayoutEffect, useRef, useState } from "react";
+import { useLayoutEffect, useRef } from "react";
 import "./navbar.scss";
+
+const hooks = [
+    "useState",
+    "useEffect",
+    "useCallback",
+    "useRef",
+    "useContext",
+    "useTransition",
+    "useDeferredValue",
+    "useReducer",
+];
 
 const Navbar = ({ changeSelectedHook }) => {
     const navbarRef = useRef(null);
+    let overflowedHooksArray = [];
 
     const checkOverflow = () => {
         hideOverflowedElements();
-    };
-
-
-    const hideOverflowedElements = () => {
-        const hooksContainerChldrens = navbarRef.current.children;
-        const containerWidth = navbarRef.current.offsetWidth;
-
-        let overflowedHooksArray = [];
-
-        if (hooksContainerChldrens) {
-            const hooksArray = Array.from(hooksContainerChldrens);
-
-            let totalWidth = 0 - 10; //minus padding one element
-            hooksArray.forEach((hook) => {
-                totalWidth += hook.offsetWidth;
-                if (totalWidth > containerWidth) {
-                    overflowedHooksArray.push(hook.textContent);
-                }
-            });
-
-            if (overflowedHooksArray) {
-                for (let i = 0; i < hooksContainerChldrens.length; i++) {
-                    overflowedHooksArray.forEach((element) => {
-                        if (hooksContainerChldrens[i].textContent === element) {
-                            hooksContainerChldrens[i].style.display = "none";
-                        }
-                    });
-                }
-            }
-        }
+        showElements();
     };
 
     useLayoutEffect(() => {
@@ -48,38 +31,68 @@ const Navbar = ({ changeSelectedHook }) => {
         };
     }, [navbarRef]);
 
+    const showElements = () => {
+        const hooksContainerChldrens = navbarRef.current.children;
+
+        if (hooksContainerChldrens) {
+            const hooksArray = Array.from(hooksContainerChldrens);
+
+            for (const hook of hooksArray) {
+                const isHookVisible = hook.style.display == "" ? true : false;
+
+                if (!isHookVisible) {
+                    hook.style.display = "";
+
+                    const newContainerWidth = navbarRef.current.offsetWidth;
+                    const containerContentWidth = navbarRef.current.scrollWidth;
+
+                    if (newContainerWidth < containerContentWidth) {
+                        hook.style.display = "none";
+                        break;
+
+                    } else {
+                        overflowedHooksArray = overflowedHooksArray.filter(
+                            (hookName) => hookName !== hook.textContent
+                        );
+                    }
+                }
+            }
+        }
+    };
+
+    const hideOverflowedElements = () => {
+        const hooksContainerChldrens = navbarRef.current.children;
+        const containerWidth = navbarRef.current.offsetWidth;
+
+        if (hooksContainerChldrens) {
+            const hooksArray = Array.from(hooksContainerChldrens);
+
+            let totalWidth = 0 - 10; //minus padding one element
+
+            hooksArray.forEach((hook) => {
+                totalWidth += hook.offsetWidth;
+
+                if (totalWidth > containerWidth) {
+                    hook.style.display = "none";
+
+                    if (!overflowedHooksArray.includes(hook.textContent)) {
+                        overflowedHooksArray.push(hook.textContent);
+                    }
+                }
+            });
+            setHooksInDropdown(overflowedHooksArray);
+        }
+    };
+
     return (
         <nav>
             <span>React Hooks Comparison</span>
             <div className="hooks-container" ref={navbarRef}>
-                <span
-                    onClick={() => changeSelectedHook("useState")}
-                    style={{ border: "1px solid red" }}
-                >
-                    useState
-                </span>
-                <span onClick={() => changeSelectedHook("useEffect")}>
-                    useEffect
-                </span>
-                <span onClick={() => changeSelectedHook("useMemo")}>
-                    useMemo
-                </span>
-                <span onClick={() => changeSelectedHook("useCallback")}>
-                    useCallback
-                </span>
-                <span onClick={() => changeSelectedHook("useRef")}>useRef</span>
-                <span onClick={() => changeSelectedHook("useContext")}>
-                    useContext
-                </span>
-                <span onClick={() => changeSelectedHook("useTransition")}>
-                    useTransition
-                </span>
-                <span onClick={() => changeSelectedHook("useDeferredValue")}>
-                    useDeferredValue
-                </span>
-                <span onClick={() => changeSelectedHook("useReducer")}>
-                    useReducer
-                </span>
+                {hooks.map((hook) => (
+                    <span key={hook} onClick={() => changeSelectedHook(hook)}>
+                        {hook}
+                    </span>
+                ))}
             </div>
         </nav>
     );
