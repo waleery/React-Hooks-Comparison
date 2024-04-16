ead1940ec14b52459440573e0bcb652fb0ece295
waleery <walenda1997@gmail.com>
2024-04-16 22:11:03 +0200

    feat(Hooks): add description to useLayoutEffect

--- src/Components/useLayoutEffect/UseLayoutEffect.jsx
+++ src/Components/useLayoutEffect/UseLayoutEffect.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useLayoutEffect, useState } from "react";
 
 const userIds = [1, 2];
 
@@ -6,14 +6,12 @@ const UseLayoutEffect = () => {
     const [userId, setUserId] = useState(userIds[0]);
     const [isAdmin, setIsAdmin] = useState(true);
 
-    //This artificially slows down rendering
     let now = performance.now();
     while (performance.now() - now < 200) {
         //do nothing for 200ms
     }
 
-    //asynchronous function -> dont block first render
-    useEffect(() => {
+    useLayoutEffect(() => {
         setIsAdmin(userId === userIds[0]);
     }, [userId]);
 
@@ -21,23 +19,37 @@ const UseLayoutEffect = () => {
         setUserId(userIds.find((id) => id !== userId));
     };
     return (
-        <div className="hookContainer">
+        <div className="hookContainer" style={{ maxWidth: "40%" }}>
             <div className="description">
-                <span className="title">With useEffect</span>
-                {/* <p className="margin-bottom">
-                    With useRef in React, we can reference elements in the DOM
-                    tree.
+                <span className="title">With useLayoutEffect</span>
+                <p className="margin-bottom">
+                    useLayoutEffect is a version of useEffect that fires before
+                    the browser repaints the screen.
                 </p>
-                <p className="margin-bottom">Update ref value won't triger rerender.</p>
-                <p>In refs, we can store the previous value of state.</p>
+                <p className="margin-bottom">
+                    useLayoutEffect runs befor ethe browser paints the screen,
+                    allowing for state updates synchronously with the browser
+                    before the user sees the new view.
+                </p>
+                <p className="margin-bottom">
+                    In this example, after changing the user, we check if the
+                    new user is an administrator (in useLayoutEffect). This
+                    demonstrates how userId and isAdmin are updated
+                    synchronously, ensuring consistency between the user's ID
+                    and their admin status.
+                </p>
                 <p>
-                    After updating the state, we update the ref with this value.
-                    Updating the ref won't trigger a rerender of the component,
-                    so the new value of the ref will not be displayed. The new
-                    value of the ref will be displayed after the next rerender
-                    of the component.
+                    However, it's important to use useLayoutEffect cautiously as
+                    it blocks the render process briefly. For long computations
+                    or DOM manipulations, this can cause delays in user
+                    interaction.
                 </p>
-                <p></p> */}
+                <p>
+                    <b>
+                        Rendering in two passes and blocking the browser hurts
+                        performance. Try to avoid this when you can.
+                    </b>
+                </p>
             </div>
             <p>userId: {userId}</p>
             <p>Admin: {isAdmin ? "true" : "false"}</p>
